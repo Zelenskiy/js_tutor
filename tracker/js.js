96c5d5ffc1331dd4eb2b525896f59541d39d57e3
Zelenskiy <Zelensk1973@gmail.com>
2020-05-16 13:40:12 +0300

    ccc

--- tracker/js.js
+++ tracker/js.js
@@ -6,6 +6,8 @@ var cH = canvas.style.height;
 cW = 500;
 cH = 688;
 
+var mmm = {0:0.2, 1:0.4, 2:0.6, 3:0.8, 4:1, 5:1.2, 6:1.4, 7:1.6, 8:1.8, 9:2, 10:2.2, 11:2.4, 12:2.6, 13:2.8, 14:3, 15:3.2, 16:3.4, 17:3.6, 18:3.8, 19:4};
+
 var x0 = 0;
 var y0 = 0;
 var wimage = 0;
@@ -19,7 +21,7 @@ if ((navigator.userAgent.match('iPhone'))|| (navigator.userAgent.match('Android'
 	platform = "Desktop";
 }
 
-var mash = 1;
+var mash = 4;
 
 // var canv = document.getElementById('c1').style.width = cW;
 // var canv = document.getElementById('c1').style.height = cH;
@@ -34,28 +36,28 @@ var mark_btn = 0;
 
 var k1 = {
 	x : 10,
-	y : -10,
+	y : -1000000,
 	color: "red",
 };
 var k2 = {
 	x : 10,
-	y : -10,
+	y : -1000000,
 	color: "red",
 };
 
 var m1 = {
 	x : 10,
-	y : -10,
+	y : -1000000,
 	color: "red",
 };
 var m2 = {
 	x : 50,
-	y : -10,
+	y : -1000000,
 	color: "green",
 };
 var m3 = {
 	x : 100,
-	y : -10,color: "blue",
+	y : -1000000,color: "blue",
 };
 
 make_base();
@@ -64,31 +66,31 @@ function draw_markers(){
 
 
 	ctx.beginPath();
-	ctx.rect(m1.x-5, m1.y-5, 10, 10);
+	ctx.rect((m1.x)*mmm[mash]-5, (m1.y)*mmm[mash]-5, 10, 10);
 	ctx.strokeStyle = m1.color;
 	ctx.stroke();
 	ctx.closePath();
 
 	ctx.beginPath();
-	ctx.rect(m2.x-5, m2.y-5, 10, 10);
+	ctx.rect(m2.x*mmm[mash]-5, m2.y*mmm[mash]-5, 10, 10);
 	ctx.strokeStyle = m2.color;
 	ctx.stroke();
 	ctx.closePath();
 
 	ctx.beginPath();
-	ctx.rect(m3.x-5, m3.y-5, 10, 10);
+	ctx.rect(m3.x*mmm[mash]-5, m3.y*mmm[mash]-5, 10, 10);
 	ctx.strokeStyle = m3.color;
 	ctx.stroke();
 	ctx.closePath();
 
 	ctx.beginPath();
-	ctx.rect(k1.x-5, k1.y-5, 10, 10);
+	ctx.rect(k1.x*mmm[mash]-5, k1.y*mmm[mash]-5, 10, 10);
 	ctx.strokeStyle = k1.color;
 	ctx.stroke();
 	ctx.closePath();
 
 	ctx.beginPath();
-	ctx.rect(k2.x-5, k2.y-5, 10, 10);
+	ctx.rect(k2.x*mmm[mash]-5, k2.y*mmm[mash]-5, 10, 10);
 	ctx.strokeStyle = k2.color;
 	ctx.stroke();
 	ctx.closePath();
@@ -104,11 +106,9 @@ function make_base(){
 	wimage = base_image.width;
 	himage = base_image.height;
 
-	console.log("wimage->"+wimage);
-	console.log("himage->"+himage);
 
 	// ctx.drawImage(base_image, x0, y0);
-	ctx.drawImage(base_image, x0, y0, wimage*mash, himage*mash);
+	ctx.drawImage(base_image, x0, y0, wimage*mmm[mash], himage*mmm[mash]);
 
 }
 
@@ -117,13 +117,13 @@ function draw_chordes(){
 
 	var ma = (m2.y-m1.y)/(m2.x-m1.x);
 	var mb = (m3.y-m2.y)/(m3.x-m2.x);
-	var x0 = (ma*mb*(m1.y-m3.y)+mb*(m1.x+m2.x)-ma*(m2.x+m3.x))/(2*(mb-ma));
-	var y0 = -1/ma*(x0-(m1.x+m2.x)/2)+(m1.y+m2.y)/2;
+	var xc0 = (ma*mb*(m1.y-m3.y)+mb*(m1.x+m2.x)-ma*(m2.x+m3.x))/(2*(mb-ma));
+	var yc0 = -1/ma*(xc0-(m1.x+m2.x)/2)+(m1.y+m2.y)/2;
 	var x12 = (m1.x+m2.x)/2;
 	var y12 = (m1.y+m2.y)/2;
 	var x23 = (m2.x+m3.x)/2;
 	var y23 = (m2.y+m3.y)/2;
-	var r = Math.sqrt((m1.x-x0)*(m1.x-x0)+(m1.y-y0)*(m1.y-y0));
+	var r = Math.sqrt((m1.x-xc0)*(m1.x-xc0)+(m1.y-yc0)*(m1.y-yc0));
 	var tmp = document.getElementById("calibr_edit").value;
 
 	tmp = tmp.replace(',','.');
@@ -134,27 +134,27 @@ function draw_chordes(){
 	document.getElementById("lbl2").innerText = "R = " + R;
 
 	ctx.beginPath();
-	ctx.moveTo(m1.x, m1.y);
+	ctx.moveTo(m1.x*mmm[mash], m1.y*mmm[mash]);
 	ctx.strokeStyle ="blue";
 	ctx.stroke();
-	ctx.lineTo(m2.x, m2.y);
+	ctx.lineTo(m2.x*mmm[mash], m2.y*mmm[mash]);
 	ctx.stroke();
 	ctx.lineWidth = 3;
 	ctx.stroke();
-	ctx.moveTo(x12, y12);
-	ctx.lineTo(x0, y0);
+	ctx.moveTo(x12*mmm[mash], y12*mmm[mash]);
+	ctx.lineTo(xc0*mmm[mash], yc0*mmm[mash]);
 	ctx.stroke()
 	ctx.closePath();
 
 	ctx.beginPath();
-	ctx.moveTo(m2.x, m2.y);
+	ctx.moveTo(m2.x*mmm[mash], m2.y*mmm[mash]);
 	ctx.strokeStyle ="red";
 	ctx.stroke();
-	ctx.lineTo(m3.x, m3.y);
+	ctx.lineTo(m3.x*mmm[mash], m3.y*mmm[mash]);
 	ctx.lineWidth = 3;
 	ctx.stroke();
-	ctx.moveTo(x23, y23);
-	ctx.lineTo(x0, y0);
+	ctx.moveTo(x23*mmm[mash], y23*mmm[mash]);
+	ctx.lineTo(xc0*mmm[mash], yc0*mmm[mash]);
 	ctx.stroke()
 	ctx.closePath();
 
@@ -162,7 +162,7 @@ function draw_chordes(){
 	ctx.beginPath();
 	ctx.lineWidth = 1;
 	ctx.strokeStyle ="green";
-	ctx.arc(x0,y0,r, 0, 2 * Math.PI);
+	ctx.arc(xc0*mmm[mash],yc0*mmm[mash],r*mmm[mash], 0, 2 * Math.PI);
 	ctx.stroke()
 	ctx.closePath();
 };
@@ -217,7 +217,16 @@ document.getElementById('btn_clear').onclick = function(){
 
 document.getElementById('btn_zoom_up').onclick = function(){
 	console.log("btn_zoom_up");
-	mash += mash*0.2;
+	mash++;
+	if (mash>19) mash = 19;
+	//Модифікація координат маркерів
+	// m1.x = (m1.x)*mmm[mash];
+	// m1.y = (m1.y)*mmm[mash];
+
+
+	//==============================
+
+
 	ctx.clearRect(0,0,cW,cH);
 	make_base();
 	draw_markers();
@@ -227,7 +236,7 @@ document.getElementById('btn_zoom_up').onclick = function(){
 };
 
 document.getElementById('btn_zoom_default').onclick = function(){
-	mash =1;
+	mash =4;
 	x0=0; y0=0;
 	ctx.clearRect(0,0,cW,cH);
 	make_base();
@@ -238,7 +247,14 @@ document.getElementById('btn_zoom_default').onclick = function(){
 };
 
 document.getElementById('btn_zoom_down').onclick = function(){
-	mash -= mash*0.2;
+	//Модифікація координат маркерів
+	// m1.x -= (m1.x)*0.2;
+	// m1.y -= (m1.y)*0.2;
+
+
+	//==============================
+	mash--;
+	if (mash<0) mash = 0;
 	ctx.clearRect(0,0,cW,cH);
 	make_base();
 	draw_markers();
@@ -384,9 +400,9 @@ if (platform=="Mobile"){
 	}
 }else {
 	canvas.onmousedown = function(event){
-		var r1 = Math.sqrt((event.offsetX - m1.x)*(event.offsetX - m1.x)+(event.offsetY - m1.y)*(event.offsetY - m1.y));
-		var r2 = Math.sqrt((event.offsetX - m2.x)*(event.offsetX - m2.x)+(event.offsetY - m2.y)*(event.offsetY - m2.y));
-		var r3 = Math.sqrt((event.offsetX - m3.x)*(event.offsetX - m3.x)+(event.offsetY - m3.y)*(event.offsetY - m3.y));
+		var r1 = Math.sqrt((event.offsetX/mmm[mash] - m1.x)*(event.offsetX/mmm[mash] - m1.x)+(event.offsetY/mmm[mash] - m1.y)*(event.offsetY/mmm[mash] - m1.y));
+		var r2 = Math.sqrt((event.offsetX/mmm[mash] - m2.x)*(event.offsetX/mmm[mash] - m2.x)+(event.offsetY/mmm[mash] - m2.y)*(event.offsetY/mmm[mash] - m2.y));
+		var r3 = Math.sqrt((event.offsetX/mmm[mash] - m3.x)*(event.offsetX/mmm[mash] - m3.x)+(event.offsetY/mmm[mash] - m3.y)*(event.offsetY/mmm[mash] - m3.y));
 		if (r1<5){
 			mark_btn = 1;
 		} else if (r2<5){
@@ -404,14 +420,14 @@ if (platform=="Mobile"){
 			if (mode == 0) {
 				var card = document.getElementById("markers");
 				if (mark_btn== 1) {
-					m1.x = event.offsetX;
-					m1.y = event.offsetY;
+					m1.x = event.offsetX/mmm[mash];
+					m1.y = event.offsetY/mmm[mash];
 				} else if (mark_btn == 2) {
-					m2.x = event.offsetX;
-					m2.y = event.offsetY;
+					m2.x = event.offsetX/mmm[mash];
+					m2.y = event.offsetY/mmm[mash];
 				} else if (mark_btn == 3) {
-					m3.x = event.offsetX;
-					m3.y = event.offsetY;
+					m3.x = event.offsetX/mmm[mash];
+					m3.y = event.offsetY/mmm[mash];
 				};
 
 
@@ -438,10 +454,26 @@ if (platform=="Mobile"){
 					draw_chordes();
 				}
 			} else if (mode == 2){
-				x0 += event.offsetX - xx0;
-				y0 += event.offsetY - yy0;
-				xx0 =  event.offsetX;
-				yy0 =  event.offsetY;
+				var evX = event.offsetX;
+				var evY = event.offsetY;
+				x0 += evX - xx0;
+				y0 += evY - yy0;
+				//корекція координат маркерів
+				m1.x += evX - xx0;
+				m1.y += evY - yy0;
+				m2.x += evX - xx0;
+				m2.y += evY - yy0;
+				m3.x += evX - xx0;
+				m3.y += evY - yy0;
+				k1.x += evX - xx0;
+				k1.y += evY - yy0;
+				k2.x += evX - xx0;
+				k2.y += evY - yy0;
+				//==========================
+
+
+				xx0 =  evX;
+				yy0 =  evY;
 				make_base();
 				draw_markers();
 				if ((m1.y > 0) && (m2.y > 0) && (m3.y > 0)) {
@@ -455,5 +487,10 @@ if (platform=="Mobile"){
 			canvas.onmousemove = null;
 			console.log("x0="+x0+"  y0="+y0);
 		}
+	}
+
+	document.getElementById('test').onclick = function () {
+		// ctx.translate(0, 20);
+		// console.log("translate(0, 20);")
 	}
 }
